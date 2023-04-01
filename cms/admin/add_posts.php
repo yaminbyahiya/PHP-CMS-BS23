b635936792b010fd545d4904169ba8c69cdba592
Yamin Bin Yahiya <yaminbyahiya@gmail.com>
2023-04-02 05:53:09 +0600

    Update

---
+++ cms/admin/add_posts.php
@@ -0,0 +1,47 @@
+<?php
+    if(isset($_POST["create_post"])){
+        $post_title=$_POST["title"];
+        $post_cat_id=$_POST["post_category_id"];
+        $post_author=$_POST["author"];
+        $post_status=$_POST["post_status"];
+        $post_image=$_FILES["image"];
+        $post_tags=$_POST["post_tags"];
+        $post_content=$_POST["post_content"];
+        $post_date=date("d-m-y");
+        $post_comment_count=4;
+    }
+?>
+
+<form action="" method="post" enctype="multipart/form-data">
+    <div class="form-group">
+        <label for="title">Post Title</label>
+        <input type="text" class="form-control" name="title">
+    </div>
+    <div class="form-group">
+        <label for="post_category">Post Category Id</label>
+        <input type="text" class="form-control" name="post_category_id">
+    </div>
+    <div class="form-group">
+        <label for="post_author">Post Author</label>
+        <input type="text" class="form-control" name="author">
+    </div>
+    <div class="form-group">
+        <label for="post_status">Post Status</label>
+        <input type="text" class="form-control" name="post_status">
+    </div>
+    <div class="form-group">
+        <label for="post_image">Post Image</label>
+        <input type="text" class="form-control" name="post_image">
+    </div>
+    <div class="form-group">
+        <label for="post_tags">Post Tags</label>
+        <input type="text" class="form-control" name="post_tags">
+    </div>
+    <div class="form-group">
+        <label for="post_content">Post Content</label>
+        <textarea class="form_control" name="post_content" id="" cols="30" rows="10"></textarea>
+    </div>
+    <div class="form-group">
+        <input class="btn btn-primary" type="submit" name="create_post" value="Publish">
+    </div>
+</form>

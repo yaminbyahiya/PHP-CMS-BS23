057c6c9f8d4f7670ca7c0cc5246f451d21687ed6
Yamin Bin Yahiya <yaminbyahiya@gmail.com>
2023-04-02 01:44:15 +0600

    Retrieving Posts from DB

--- cms/admin/posts.php
+++ cms/admin/posts.php
@@ -33,17 +33,38 @@
                                 </tr>
                             </thead>
                             <tbody>
-                                <tr>
-                                    <td>a</td>
-                                    <td>a</td>
-                                    <td>a</td>
-                                    <td>a</td>
-                                    <td>a</td>
-                                    <td>a</td>
-                                    <td>a</td>
-                                    <td>a</td>
-                                    <td>a</td>
-                                </tr>
+                                <?php
+                                    $query = "SELECT * FROM posts";
+                                    $result = mysqli_query($connection, $query);
+                                    if(!$result){
+                                        die("Query Failed". mysqli_error());
+                                    }else{
+                                        while($row = mysqli_fetch_assoc($result)){
+                                            $id=$row["id"];
+                                            $cat_id=$row["category_id"];
+                                            $post_author=$row["post_author"];
+                                            $post_title=$row["post_title"];
+                                            $post_date=$row["post_date"];
+                                            $post_image=$row["post_image"];
+                                            $post_content=$row["post_content"];
+                                            $post_tags=$row["post_tags"];
+                                            $post_comment_count=$row["post_comment_count"];
+                                            $post_status=$row["post_status"];
+                                            echo "<tr>";
+                                            echo "<td>$id</td>";
+                                            echo "<td>$post_author</td>";
+                                            echo "<td>$post_title</td>";
+                                            echo "<td>$cat_id</td>";
+                                            echo "<td>$post_status</td>";
+                                            echo "<td>$post_image</td>";
+                                            echo "<td>$post_tags</td>";
+                                            echo "<td>$post_comment_count</td>";
+                                            // echo "<td>$post_content</td>";
+                                            echo "<td>$post_date</td>";
+                                        echo "</tr>";
+                                        }
+                                    }
+                                ?>
                             </tbody>
                         </table>
                     </div>

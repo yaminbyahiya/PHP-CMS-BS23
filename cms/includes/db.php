0876bef9c486eda1e357ecf6fcb651caf5b8ea4e
Yamin Bin Yahiya <yaminbyahiya@gmail.com>
2023-04-04 02:10:37 +0600

    Update db.php

--- cms/includes/db.php
+++ cms/includes/db.php
@@ -1,15 +1,13 @@
 <?php
-    $db["host"] = "localhost";
-    $db["user"] = "root";
-    $db["pass"] = "";
-    $db["name"] = "cms";
-    foreach($db as $key => $value){
-        define(strtoupper($key), $value);
-    }
-    $connection = mysqli_connect(HOST, USER, PASS, NAME);
-    if($connection){
-        echo "We are connected!";
-    }else{
+    // $db["host"] = "localhost";
+    // $db["user"] = "root";
+    // $db["pass"] = "";
+    // $db["name"] = "cms";
+    // foreach($db as $key => $value){
+    //     define(strtoupper($key), $value);
+    // }
+    $connection = mysqli_connect("localhost", "root", "", "cms");
+    if(!$connection){
         echo "Connection failed!";
     }
 ?>

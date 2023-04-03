655b65820a5b868fc674dfd3ed9f402e6949e08e
Yamin Bin Yahiya <yaminbyahiya@gmail.com>
2023-04-03 11:49:57 +0600

    Dropdown Module on Add Category Page

--- cms/admin/add_posts.php
+++ cms/admin/add_posts.php
@@ -1,7 +1,7 @@
 <?php
     if(isset($_POST["create_post"])){
         $post_title=$_POST["title"];
-        $post_cat_id=$_POST["post_category_id"];
+        $post_cat_id=$_POST["post_category"];
         $post_author=$_POST["author"];
         $post_status=$_POST["post_status"];
         $post_image=$_FILES["image"]["name"];
@@ -23,8 +23,17 @@
         <input type="text" class="form-control" name="title">
     </div>
     <div class="form-group">
-        <label for="post_category">Post Category Id</label>
-        <input type="text" class="form-control" name="post_category_id">
+        <select name="post_category" id="">
+            <?php
+                $query="SELECT * FROM category";
+                $result=mysqli_query($connection, $query);
+                while($row=mysqli_fetch_assoc($result)){
+                    $cat_id=$row["cat_id"];
+                    $cat_title=$row["cat_title"];
+                    echo "<option value='$cat_id'>$cat_title</option>";
+                }
+            ?>
+        </select>
     </div>
     <div class="form-group">
         <label for="post_author">Post Author</label>

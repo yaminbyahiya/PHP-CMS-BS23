c7fd4198b293fb9411d2b6c22b62087a79f1f13e
Yamin Bin Yahiya <yaminbyahiya@gmail.com>
2023-04-04 22:36:51 +0600

    User Session

--- cms/admin/includes/header.php
+++ cms/admin/includes/header.php
@@ -7,10 +7,8 @@
     include "./functions.php";
 ?>
 <?php 
-    if(isset($_SESSION["user_role"])){
-        if($_SESSION["user_role"] !== "admin"){
-            header("Location: ../index.php");
-        }
+    if(!isset($_SESSION["user_role"])){
+        header("Location: ../index.php");
     }
 ?>
 <!DOCTYPE html>

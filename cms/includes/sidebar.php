6c52a0ee46bc3390602f47411a3af0ae4af7a834
Yamin Bin Yahiya <yaminbyahiya@gmail.com>
2023-04-04 17:24:25 +0600

    Login Validation Module

--- cms/includes/sidebar.php
+++ cms/includes/sidebar.php
@@ -35,6 +35,24 @@
                         <!-- /.input-group -->
                 </div>
 
+                <div class="well">
+                    <h4>Login</h4>
+                    <form action="includes/login.php" method="post">
+                        <div class="form-group">
+                            <input type="text" name="username" class="form-control" placeholder="Enter username">
+                        </div>
+                        <div class="input-group">
+                            <input type="password" name="password" class="form-control" placeholder="Enter password">
+                            <span class="input-group-btn">
+                                <button name="login" class="btn btn-primary" type="submit">
+                                    Submit
+                            </button>
+                            </span>
+                        </div>
+                    </form>
+                        <!-- /.input-group -->
+                </div>
+
 
                 <!-- Blog Categories Well -->
                 <?php

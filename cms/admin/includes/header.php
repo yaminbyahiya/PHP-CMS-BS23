3030231382f8915fbbf1e199cd4a0372f7ff6aad
Yamin Bin Yahiya <yaminbyahiya@gmail.com>
2023-04-04 17:56:35 +0600

    Session Module

--- cms/admin/includes/header.php
+++ cms/admin/includes/header.php
@@ -1,4 +1,7 @@
-<?php ob_start(); ?>
+<?php 
+    ob_start(); 
+    session_start();
+?>
 <?php 
     include "../includes/db.php";
 ?>
--- cms/includes/login.php
+++ cms/includes/login.php
@@ -1,5 +1,6 @@
 <?php 
     include "db.php";
+    session_start();
     if(isset($_POST["login"])){
         $username=$_POST["username"];
         $password=$_POST["password"];
@@ -21,6 +22,10 @@
         if($username !== $db_username && $password !== $db_password){
             header("Location: ../index.php");
         }else if($username == $db_username && $password == $db_password){
+            $_SESSION["username"]=$db_username;
+            $_SESSION["firstname"]=$db_user_firstname;
+            $_SESSION["lastname"]=$db_user_lastname;
+            $_SESSION["user_role"]=$db_user_role;
             header("Location: ../admin");
         }else{
             header("Location: ../index.php");

e4a8a7a2462a2292d221518cef8ad153eafb1540
Yamin Bin Yahiya <yaminbyahiya@gmail.com>
2023-05-02 12:19:17 +0600

    Fetching Posts using PREPARE statement

--- cms/category.php
+++ cms/category.php
@@ -1,6 +1,7 @@
 <?php 
     include "includes/db.php";
     include "includes/header.php";
+    include "admin/functions.php";
 ?>
     <!-- Navigation -->
     
@@ -20,22 +21,44 @@
                     if(isset($_GET["category"])){
                         $category_id=$_GET["category"];
                     }
-                    if(isset($_SESSION["user_role"]) && $_SESSION["user_role"]=="admin"){
-                        $query = "SELECT * FROM posts WHERE category_id=$category_id";
+                    // if(is_admin($_SESSION["username"])){
+                    //     $query = "SELECT * FROM posts WHERE category_id=$category_id";
+                    // }else{
+                    //     $query = "SELECT * FROM posts WHERE category_id=$category_id AND post_status='Published'";
+                    // }
+                    if(is_admin($_SESSION["username"])){ //Alternative approach using PREPARE statements
+                        $stm1 = mysqli_prepare($connection, "SELECT id, post_title, post_author, post_date, post_image, post_content FROM posts WHERE category_id=?");
                     }else{
-                        $query = "SELECT * FROM posts WHERE category_id=$category_id AND post_status='Published'";
+                        $stmt2 = mysqli_prepare($connection, "SELECT id, post_title, post_author, post_date, post_image, post_content FROM posts WHERE category_id=? AND post_status=?");
+                        $published = "published";
                     }
-                    $result = mysqli_query($connection, $query);
-                    if(mysqli_num_rows($result) < 1){
+                    if(isset($stm1)){
+                        mysqli_stmt_bind_param($stm1, "i", $category_id);
+                        mysqli_stmt_execute($stm1);
+                        mysqli_stmt_bind_result($stm1, $id, $post_title, $post_author, $post_date, $post_image, $post_content);
+                        $stmt = $stm1;
+                    }else{
+                        mysqli_stmt_bind_param($stmt2, "is", $category_id, $published);
+                        mysqli_stmt_execute($stmt2);
+                        mysqli_stmt_bind_result($stmt2, $id, $post_title, $post_author, $post_date, $post_image, $post_content);
+                        $stmt = $stmt2;
+                    }
+                    // $result = mysqli_query($connection, $query);
+
+                    // if(mysqli_num_rows($result) < 1){
+                    //     echo "<h1 class='text-center'>No Post Found</h1>";
+                    // }
+                    if(mysqli_stmt_num_rows($stmt) === 0){
                         echo "<h1 class='text-center'>No Post Found</h1>";
                     }
-                    while($row = mysqli_fetch_assoc($result)){
-                        $post_id=$row["id"];
-                        $post_title = $row["post_title"];
-                        $post_author = $row["post_author"];
-                        $post_date = $row["post_date"];
-                        $post_image = $row["post_image"];
-                        $post_content = substr($row["post_content"],0,100);
+                    // while($row = mysqli_fetch_assoc($result)){
+                    //     $post_id=$row["id"];
+                    //     $post_title = $row["post_title"];
+                    //     $post_author = $row["post_author"];
+                    //     $post_date = $row["post_date"];
+                    //     $post_image = $row["post_image"];
+                    //     $post_content = substr($row["post_content"],0,100);
+                    while(mysqli_stmt_fetch($stmt)){
 
                         ?>
                         

3beb073631c026a9e42535eac00fcaf9ee577210
Yamin Bin Yahiya <yaminbyahiya@gmail.com>
2023-05-03 18:25:27 +0600

    Addition of Helper Functions

--- cms/includes/navigation.php
+++ cms/includes/navigation.php
@@ -11,7 +11,8 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="./index.php">CMS Front</a>
+                <!-- <a class="navbar-brand" href="./index.php">CMS Front</a> -->
+                <a class="navbar-brand" href="./index">CMS Front</a>
             </div>
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
